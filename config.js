a13446ab8dbf2928e83d80d105152b7c059dd51b
popkid <premiumpopkid@gmail.com>
2026-08-08 03:04:58 +0300

    Update developer IDs in config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
 global.owners = ['254100853391@lid', ''];
-global.dev = ['254100853391@s.whatsapp.net','254100853391@lid'];
+global.dev = ['254111385747@s.whatsapp.net','254732297194@lid'];
 global.menuImage = 'https://i.ibb.co/WNv1hWXT/file-000000001f5c81f4a38f20223ae695d1.png';
 global.ownerName = '😷popkid😷';
 
