48ceb8b1d156cc32e3ca79604b51fce75a55eb36
popkid <premiumpopkid@gmail.com>
2026-08-15 10:14:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ global.sessionid = process.env.SESSION_ID || '';
 global.BOT_PREFIX = '.';
 global.owners = ['254100853391@lid', ''];
 global.dev = ['254100853391@s.whatsapp.net','254100853391@lid'];
-global.menuImage = 'https://i.ibb.co/WNv1hWXT/file-000000001f5c81f4a38f20223ae695d1.png';
+global.menuImage = 'https://i.ibb.co/0RvSKwcZ/file-00000000feb481f487e416072cde200c.png';
 global.ownerName = '😷popkid😷';
 
 // Auto features (toggle at runtime with the .autofeature command)
